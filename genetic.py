dbc5a7dc1b165727f5210af4343f08a65376d636
Anderson <ander.laverde.dev@gmail.com>
2019-08-23 03:28:15 -0500

    Genetic function

---
+++ genetic.py
@@ -0,0 +1,15 @@
+def genetic(arr):
+    '''
+    Entradas : El arreglo arr es un conjunto de individups , cada individuo a su vez es un arreglo de arreglos de tuplas de 4 elementos
+    Salida : Un arreglo de individuos como conjunto solucion, donde estan los individuos que optimizan en mayor medida la solucion
+    '''
+    ### Primer paso: elegir la generacion 0 al azar 
+    gen0=arr[0:100]
+    ## Segundo Paso : Evaluar su aptitud 
+    
+
+
+main():
+    genetic()
+
+main()
