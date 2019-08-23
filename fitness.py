7e0fa68dcfa286ce4199fbb159a6feb82f4ebecf
Anderson <ander.laverde.dev@gmail.com>
2019-08-22 20:59:33 -0500

    first commit with comments

---
+++ fitness.py
@@ -0,0 +1,5 @@
+##Fitness Function 
+##La estructura de mi individuo esta dada por una lista de listas en la cual en cada sublista hay una tupla con las caracteristicas (maquina,operacion,t inicio, tfinal)
+
+function fitness():
+    
